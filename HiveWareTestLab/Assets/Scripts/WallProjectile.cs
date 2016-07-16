d4e79a83a25917c2c9a37905bad7d86c5833f880
Cameron Unknown <Cameron Unknown>
2016-07-16 17:37:38 -0600

    ArrowHole can now go onto wall

--- HiveWareTestLab/Assets/Scripts/WallProjectile.cs
+++ HiveWareTestLab/Assets/Scripts/WallProjectile.cs
@@ -4,10 +4,17 @@ using System.Collections;
 public class WallProjectile : MonoBehaviour {
 
     public float speed;
-    public float WallImmuneTime;
+    public float delay;
+
+    private bool firstUpdate = true;
 
     void Update()
     {
+        if (firstUpdate)
+        {
+            StartCoroutine(DelayCollider());
+        }
+
         if (Globals.notFrozen)
         {
             transform.Translate(Vector2.down * speed * Time.deltaTime);
@@ -18,5 +25,18 @@ public class WallProjectile : MonoBehaviour {
     {
         GameObject.Find("GameController").SendMessage("HurtPlayer", Vector3.Normalize(GameObject.Find("Player").transform.position - transform.position));
         Destroy(gameObject);
+    }
+
+    private IEnumerator DelayCollider()
+    {
+        float time = Time.time + delay;
+
+        while(time > Time.time)
+        {
+            yield return null;
+        }
+
+        GetComponent<BoxCollider2D>().enabled = true;
+
     }
 }
