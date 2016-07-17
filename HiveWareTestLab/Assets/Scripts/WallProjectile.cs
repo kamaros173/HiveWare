fb35e293698c150dc50ca71718553a1d1d5ef545
Cameron Unknown <Cameron Unknown>
2016-07-16 18:36:58 -0600

    Fixed Wall Arrow a bit

--- HiveWareTestLab/Assets/Scripts/WallProjectile.cs
+++ HiveWareTestLab/Assets/Scripts/WallProjectile.cs
@@ -5,19 +5,41 @@ public class WallProjectile : MonoBehaviour {
 
     public float speed;
     public float delay;
+    public LayerMask Shield;
 
     private bool firstUpdate = true;
+    private RaycastHit2D hit;
+    private Vector3 shotDirection;
+
+    void Start()
+    {         
+        shotDirection = Vector3.Normalize(GameObject.Find("Player").transform.position - transform.position);
+    }
 
     void Update()
     {
-        if (firstUpdate)
-        {
-            StartCoroutine(DelayCollider());
-        }
-
         if (Globals.notFrozen)
         {
-            transform.Translate(Vector2.down * speed * Time.deltaTime);
+            float traveled = 2.5f * speed * Time.deltaTime;
+
+            hit = Physics2D.Raycast(transform.position, shotDirection, traveled, Shield);
+            if (hit.collider != null)
+            {
+                GetComponent<BoxCollider2D>().enabled = false;
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+            }
+
+        }
+
+        if (firstUpdate)
+        {
+            firstUpdate = false;
+            StartCoroutine(DelayCollider());
         }
     }
 
